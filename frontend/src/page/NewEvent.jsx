fe40bcb82b58adc234e7ddaac3b6b900e43df15c
Raíssa <raissajoaquim29@gmail.com>
2025-08-21 22:14:39 -0300

    validando input , output, erros do componente new

--- frontend/src/page/NewEvent.jsx
+++ frontend/src/page/NewEvent.jsx
@@ -3,14 +3,7 @@ import { redirect } from 'react-router-dom';
 import { json } from '@remix-run/router';
 
 function NewEvent() {
-
-    function submitHandler(event) {
-        event.preventDefaul();
-
-    }
-
-    return <EventForm />
-
+    return <EventForm />;
 }
 
 export default NewEvent;
@@ -32,6 +25,10 @@ export async function action({ request, params }) {
         },
         body: JSON.stringify(eventData),
     });
+
+    if (response.status === 422) {
+        return response;
+    }
 
     if (!response.ok) {
         throw json({ message: 'Could not save event.' }, { status: 500 });
